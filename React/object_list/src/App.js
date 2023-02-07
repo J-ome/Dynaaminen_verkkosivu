0ee202955d811c2c3c3efd3b6095beea482ec916
J-ome <n2amjo00@students.oamk.fi>
2023-02-07 10:03:10 +0200

    object list tehtävä

--- React/object_list/src/App.js
+++ React/object_list/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import Item from './Item.js';
 import './App.css';
+import { useEffect, useState } from 'react';
+import uuid from 'react-uuid';
 
 function App() {
+
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const shoppingList = Array()
+    shoppingList.push(new Item("Coffee", 1))
+    shoppingList.push(new Item("Cookies", 5))
+    shoppingList.push(new Item("Milk", 2))
+    setItems(shoppingList)
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Shopping List</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <tr key={uuid()}>
+              <td>{item.name}</td>
+              <td>{item.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
