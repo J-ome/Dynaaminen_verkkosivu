e5511f09014192be26b516ab49ec52e20843c1af
J-ome <n2amjo00@students.oamk.fi>
2023-02-07 10:49:09 +0200

    routing

--- React/routing/src/App.js
+++ React/routing/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
 
-
-  
   return (
-    
+    <>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
