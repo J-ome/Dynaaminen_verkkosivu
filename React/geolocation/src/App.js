2c57fc7abece1580e3edd37eeb5b04252d591442
J-ome <n2amjo00@students.oamk.fi>
2023-02-16 11:41:17 +0200

    Geolocation harjoitus

---
+++ React/geolocation/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [lat, setLat] = useState(0)
+  const [lng, setLng] = useState(0)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude)
+        setLng(position.coords.longitude)
+      }, (error) => {
+        alert(error)
+      })
+    }
+    else {
+      alert("Your browser does not support geolocation")
+    }
+  }, [])
+  
+
+  return (
+    <p>
+      Position: <br></br>
+      Latitude: {lat.toFixed(3)}<br></br>
+      Longitude: {lng.toFixed(3)}
+    </p>
+  );
+}
+
+export default App;
