56eb07f427f5c32a5bb3c23a6ea1be6733d8ef4b
J-ome <n2amjo00@students.oamk.fi>
2023-02-16 09:11:31 +0200

    Joke of the day tehtävä JSON

---
+++ React/jokeoftheday/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+
+
+const URL = "https://api.jokes.one/jod"
+
+function App() {
+
+  const [title, setTitle] = useState("")
+  const [text, setText] = useState("")
+
+  useEffect(() => {
+    async function getJoke() {
+      try {
+        const response = await fetch(URL)
+        if (response.ok) {
+          const json = await response.json()
+          const joke = json.contents.jokes[0].joke
+          setTitle(joke.title)
+          setText(joke.text)
+        } else {
+          alert("Error retrieving joke!")
+        }
+      } catch (err) {
+        alert(err)        
+      }
+    }
+    getJoke()
+   
+  }, [])
+  
+
+
+  return (
+    <div>
+      <h1>Joke of the day!</h1>
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
