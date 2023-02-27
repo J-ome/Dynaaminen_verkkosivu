2873eac166296cdcd452b2ecabf19b3e8d0e22d6
J-ome <n2amjo00@students.oamk.fi>
2023-02-27 19:52:36 +0200

    eka commit

---
+++ React/toinen_palautustyo/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+const API_URL = "www.thecocktaildb.com/api/json/v1/1/search.php?s="
+const API_KEY = "1"
+
+
+function App() {
+
+  
+
+
+  return (
+    <>
+      <h2>Coctail of the day</h2>
+
+        <input></input>
+        <button>Search</button>
+
+        <h3>Coctail name</h3>
+        <h4>Glass</h4>
+        <p>What glass</p>
+        <h4>Instructions</h4>
+        <p>Instructions here</p>
+        <ul>
+          <li>lista aineksista</li>
+        </ul>
+
+    </>
+  );
+}
+
+export default App;
